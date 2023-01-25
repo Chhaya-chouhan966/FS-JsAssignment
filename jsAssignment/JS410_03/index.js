9ffeeba8c5c51c3d1da09ecad5e8d0c3f1330250
Chhaya-chouhan966 <107814362+Chhaya-chouhan966@users.noreply.github.com>
2023-01-26 00:10:07 +0530

    updated

---
+++ jsAssignment/JS410_03/index.js
@@ -0,0 +1,24 @@
+
+const input=document.getElementById("input")
+const resultContainer=document.getElementById("resultContainer")
+
+async function enter() {
+    resultContainer.innerHTML=""
+    const response=await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${input.value}`)
+    const data=await response.json()
+
+         const arr=[]
+         data.meals.map((ele)=>{
+            arr.push(ele.strMealThumb)
+        })
+
+        arr.map((ele)=>{
+            const h=ele.startsWith(input.value)
+            if (h==false) {
+                resultContainer.innerHTML+=`<img id="img" src="${ele}" alt="">`     
+                
+            }
+        })
+
+
+}
