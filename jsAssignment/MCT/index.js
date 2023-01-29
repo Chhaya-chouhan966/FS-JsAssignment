1235a7d8c7463feac40b54611930c19ad2900b61
Chhaya-chouhan966 <107814362+Chhaya-chouhan966@users.noreply.github.com>
2023-01-29 18:31:59 +0530

    mct

--- jsAssignment/MCT/index.js
+++ jsAssignment/MCT/index.js
@@ -30,5 +30,27 @@ async function search() {
                      <h4>Feel like : ${data.main.feels_like}</h4>
                      
                      </div>`
+
+  const other=document.getElementById("other")
+  const weekly_data=["sunday", "monday", "tuesday", "wednsday", "thrusday", "friday", "satarday"]
+
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  
+  for (let i = 0; i < weekly_data.length; i++) {
+       const day=new Date()
+       console.log(day);
+    //    var datas=weekly_data[day.getDay()]
+    //    console.log(datas);
+
+    
+    other.innerHTML=`    <h4 class="head4">${input.value}</h4>
+                         <h4 class="head4">${weekly_data[day.getDay()]}</h4>
+                         <h4 class="head4">${day.getDate()} ${months[day.getMonth()+1]} ${day.getFullYear()}</h4>
+                         <h4 class="head4">${day.getHours()+":"+day.getMinutes()+":"+day.getSeconds()}</h4>
+                         <h4 class="head4">${day}</h4>
+                         `
+  }  
+  
+  
 }
 
